372a3d08ccfd09fb2ea38af8bc0e1a73eccdcf4a
MATHEUS DANTAS VIEIRA <ma_dv@outlook.com.br>
2026-09-22 13:58:57 -0300

    Style: Modelagem do footer
    Footer antigo foi remodelado, foi removido o front de novo usuário, adicionado um Music Media Player no footer, para se assemelhar ao spotify.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Footer from './components/Footer/Footer';
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,59 @@
 import React from "react";
 import "./Footer.css";
-
+import img from '../../assets/playlist/1.jpeg';
 
 const Footer = () => {
     return(
         <footer className="disclaimer-premium">
-            <div className="text">
-            <p className="disclaimer-premium__title">Testar o premium de graça por 3 meses</p>
-            <p className="disclaimer-premium__subtitle">Inscreva-se para curtir música sem anúncios, off-line e muito mais.</p>
-    </div>
-    <div className="button">
-        <button Type="disclaimer-premium__button">Inscreva-se grátis</button>	
-    </div>
-    </footer>
+            <div className="disclaimer-premium-music-container">
+                <div className="disclaimer-premium-music-logo">
+                    <img src={img} alt="Spotify Logo" />
+                </div>
+
+                <div className="disclaimer-premium-music-info">
+                    <div class="disclaimer-premium-music-info-text">
+                        <h4 id="disclaimer-premium-title">Music</h4>
+                        <h5 id="disclaimer-premium-subtitle">Music band</h5>
+                    </div>
+                    <button className="disclaimer-premium-music-additional-controls-button">
+                        <i className="bi bi-heart"></i>
+                    </button>
+                </div>
+
+                <div className="disclaimer-premium-music-progress">
+                    <input type="range" className="disclaimer-premium-music-progress-bar" />
+                </div>
+                <div className="disclaimer-premium-music-controls">
+                    <button className="disclaimer-premium-music-controls-button">
+                        <i className="bi bi-shuffle"></i>
+                    </button>
+                    <button className="disclaimer-premium-music-controls-button">
+                        <i className="bi bi-skip-backward-fill"></i>
+                    </button>
+                    <button className="disclaimer-premium-music-controls-play">
+                        <i className="bi bi-play-circle-fill"></i>
+                    </button>
+                    <button className="disclaimer-premium-music-controls-button">
+                        <i className="bi bi-skip-forward-fill"></i>
+                    </button>
+                    <button className="disclaimer-premium-music-controls-button">
+                        <i className="bi bi-repeat"></i>
+                    </button>
+                </div>
+                <div className="disclaimer-premium-music-volume">
+                    <i className="bi bi-volume-up"></i>
+                    <input type="range" className="disclaimer-premium-music-volume-bar" />
+                </div>
+                <div className="disclaimer-premium-music-additional-controls">
+                    <button className="disclaimer-premium-music-additional-controls-button">
+                        <i className="bi bi-list"></i>
+                    </button>
+                    <button className="disclaimer-premium-music-additional-controls-button">
+                        <i className="bi bi-fullscreen"></i>
+                    </button>
+                </div>
+            </div>
+        </footer>
     )
 };
 
